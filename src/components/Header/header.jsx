d6a2416587bcfa1d8a95beb2547505671f0889cf
RicardoVegaJr <rvega921@gmail.com>
2024-06-26 15:43:56 -0500

    Complete Header Structure

--- src/components/Header/header.jsx
+++ src/components/Header/header.jsx
@@ -1,5 +1,20 @@
+import "./header.css";
+import avatar from "../../assets/avatar.svg";
+import logo from "../../assets/logo.svg";
+
 function Header() {
-  return <header>Hello</header>;
+  return (
+    <header className="header">
+      <img src={logo} alt="" className="header__logo" />
+      <p className="header__date-and-location">Date, Location</p>
+
+      <button className="header__add-clothes-btn">+ Add Clothes</button>
+      <div className="header_user-container">
+        <p className="header__username">Terrence Tegegne</p>
+        <img src={avatar} alt="Terrenece Tegegne" className="header__avatar" />
+      </div>
+    </header>
+  );
 }
 
 export default Header;
