4479512d40a3fc5b6d9daae9ed9ab2bfb5875643
Ricardo Vega <rvega921@gmail.com>
2024-09-24 21:33:40 -0500

    Feature: Create F & C switch functionality

--- src/components/Header/header.jsx
+++ src/components/Header/header.jsx
@@ -1,16 +1,18 @@
 import "./header.css";
 import avatar from "../../assets/avatar.svg";
 import logo from "../../assets/logo.svg";
+import ToggleSwitch from "../ToggleSwitch/ToggleSwitch";
 
 function Header({ handleAddClick, weatherData }) {
 console.log(weatherData);
 const currentDate = new Date().toLocaleString('default', { month: 'long', day: 'numeric' });
 
   return (
-    <header className="header">
+    <header className="header">.
       <img src={logo} alt="logo" className="header__logo" />
       <p className="header__date-and-location">{currentDate}, {weatherData.city}</p>
-
+      
+      <ToggleSwitch/>
       <button
         onClick={handleAddClick}
         type="button"
@@ -18,6 +20,7 @@ const currentDate = new Date().toLocaleString('default', { month: 'long', day: '
       >
         + Add Clothes
       </button>
+      
       <div className="header_user-container">
         <p className="header__username">Terrence Tegegne</p>
         <img src={avatar} alt="Terrenece Tegegne" className="header__avatar" />
