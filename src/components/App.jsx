a382ebb5828417ad20f0a92750ab7fd25601b461
Ricardo Vega <rvega921@gmail.com>
2025-07-30 13:39:15 -0400

    Delete: data pass in registration.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -81,7 +81,7 @@ function App() {
       .then(() => {
         closeActiveModal();
         return authorize(email, password);
-      }).then((data) =>{
+      }).then(() =>{
         handleLogin({email, password});
       })
       .catch(console.error);
